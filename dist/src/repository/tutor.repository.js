a5e313dc6b91698c6f5599e84d8f8a8bf5720e8d
Thiago Vieira <60758912+ThiagoVieira585@users.noreply.github.com>
2023-06-29 19:16:02 -0300

    tirando a pasta dist -3

--- dist/src/repository/tutor.repository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TutorRepository = void 0;
-class TutorRepository {
-}
-exports.TutorRepository = TutorRepository;
