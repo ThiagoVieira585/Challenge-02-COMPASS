d42d9f7e566ad4962fc9234aa48befe833c3fa41
Thiago Vieira <60758912+ThiagoVieira585@users.noreply.github.com>
2023-06-29 20:32:49 -0300

    rota update criada

--- src/routes/tutor.route.ts
+++ src/routes/tutor.route.ts
@@ -1,6 +1,6 @@
 import express, { json } from "express";
 import { Request, Response } from "express";
-import { createTutor, getById, readTutor } from "../controller/tutor.controler";
+import { createTutor, deleteById, getById, readTutor, updateById } from "../service/tutor.service";
 
 const router = express.Router();
 router.use(express.json())
@@ -11,7 +11,10 @@ router.get("/tutors/:id", getById, (req: Request, res: Response) =>{
 });
 router.post("/tutors", createTutor, (req: Request, res: Response) =>{
 });
-
+router.delete("/tutors/:id", deleteById, (req: Request, res: Response) =>{
+})
+router.put("/tutors/:id", updateById, (req: Request, res: Response) =>{
+})
 
 
 export default router;
---
+++ src/service/tutor.service.ts
@@ -0,0 +1,76 @@
+import { Request, Response } from "express";
+import Tutor from "../model/tutor.model";
+
+export async function createTutor(req: Request, res: Response) {
+  const { name, phone, email, date_of_birth, zip_code, pets } = req.body;
+
+  const tutor = {
+    name,
+    phone,
+    email,
+    date_of_birth,
+    zip_code,
+    pets,
+  };
+
+  try {
+    await Tutor.create(tutor);
+
+    res.status(201).json({ message: "Sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+}
+
+export async function readTutor(req: Request, res: Response) {
+  try {
+    const tutor = await Tutor.find();
+
+    res.status(200).json(tutor);
+  } catch (error) {}
+}
+
+export async function getById(req: Request, res: Response) {
+  const id = req.params.id;
+  try {
+    const tutor = await Tutor.findOne({ _id: id });
+
+    res.status(200).json(tutor);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+}
+
+export async function deleteById(req: Request, res: Response) {
+  const id = req.params.id;
+  try {
+    const tutor = await Tutor.deleteOne({ _id: id });
+    res.status(204).json({ tutor, message: "Tutor apagado" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+}
+
+export async function updateById(req: Request, res: Response) {
+  const id = req.params.id;
+
+  const { name, phone, email, date_of_birth, zip_code, pets } = req.body;
+
+  const tutors = {
+    name,
+    phone,
+    email,
+    date_of_birth,
+    zip_code,
+    pets,
+  };
+  if (!name || !phone || !email || !date_of_birth || !zip_code || !pets) {
+    res.status(400).json({ error: "Todos os campos são obrigatórios" });
+    return;
+  }
+  try {
+    const tutor = await Tutor.updateOne({ _id: id }, tutors);
+
+    res.status(200).json(tutor);
+  } catch (error) {}
+}
