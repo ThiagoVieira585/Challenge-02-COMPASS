b1dc59f46830e9bcb7b0b7f9e6a8f35861390811
Thiago Vieira <60758912+ThiagoVieira585@users.noreply.github.com>
2023-06-30 17:22:41 -0300

    rota delete do pet e rota atualizada

--- src/routes/pet.route.ts
+++ src/routes/pet.route.ts
@@ -1,12 +1,14 @@
 import express, { json } from "express";
 import { Request, Response } from "express";
-import { createPet } from "../service/pet.service";
+import { createPet, updatePet, deletePet } from "../service/pet.service";
 
 const router = express.Router();
 router.use(express.json())
 
 router.post("/pet/:tutorId", createPet, (req:Request, res:Response) =>{
-})
-router.put("/pet/:petId/tutor/:tutorId", (req:Request, res:Response) =>{
-})
+});
+router.put("/pet/:petId/tutor/:tutorId", updatePet, (req:Request, res:Response) =>{
+});
+router.delete("/pet/:petId/tutor/:tutorId", deletePet, (req:Request, res:Response) =>{
+});
 export default router;
--- src/service/pet.service.ts
+++ src/service/pet.service.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
-import { _Pet } from '../model/pet.model';
-import Tutor, { _Tutor } from '../model/tutor.model';
-import Pet  from '../model/pet.model';
+import { Request, Response } from "express";
+import { _Pet } from "../model/pet.model";
+import Tutor, { _Tutor } from "../model/tutor.model";
+import Pet from "../model/pet.model";
 
 export async function createPet(req: Request, res: Response) {
   const tutorId = req.params.tutorId;
@@ -9,11 +9,13 @@ export async function createPet(req: Request, res: Response) {
   const { name, species, category, weight, date_of_birth } = req.body;
 
   if (!name || !species || !category || !weight || !date_of_birth) {
-    return res.status(400).json({ error: 'Todos os campos requeridos devem ser fornecidos.' });
+    return res
+      .status(400)
+      .json({ error: "Todos os campos requeridos devem ser fornecidos." });
   }
 
   try {
-    const pets = {
+    const petData = {
       name,
       species,
       category,
@@ -21,19 +23,75 @@ export async function createPet(req: Request, res: Response) {
       date_of_birth,
     };
 
-    const pet = new Pet(pets);
+    const pet = new Pet(petData);
     await pet.save();
 
     const tutor: _Tutor | null = await Tutor.findById(tutorId);
 
     if (!tutor) {
-      return res.status(404).json({ error: 'Tutor não encontrado.' });
+      return res.status(404).json({ error: "Tutor não encontrado." });
     }
 
-    tutor.pets.push(pet._id);
+    tutor.pets.push(pet);
     await tutor.save();
 
-    res.status(201).json(pet);
+    res.status(201).json({ pet: pet.toObject(), message: "Pet criado" });
+
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+}
+
+
+export async function updatePet(req: Request, res: Response) {
+  const idTutor = req.params.tutorId;
+  const idPet = req.params.petId;
+
+  const { name, species, category, weight, date_of_birth } = req.body;
+
+  const pets = {
+    name,
+    species,
+    category,
+    weight,
+    date_of_birth,
+  };
+
+  if (!name || !species || !category || !weight || !date_of_birth) {
+    return res
+      .status(400)
+      .json({ error: "Todos os campos requeridos devem ser preenchidos." });
+  }
+
+  try {
+    const pet = await Pet.updateOne({ _id: idPet }, pets);
+
+    res.status(200).json({ pet, message: "Pet atualizado" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+}
+export async function deletePet(req: Request, res: Response) {
+  const tutorId = req.params.tutorId;
+  const petId = req.params.petId;
+
+  try {
+    const tutor = await Tutor.findById(tutorId);
+    if (!tutor) {
+      return res.status(404).json({ error: 'Tutor não encontrado' });
+    }
+
+    const petIndex = tutor.pets.findIndex((pet: _Pet) => pet._id.toString() === petId);
+    if (petIndex === -1) {
+      return res.status(404).json({ error: 'Pet não encontrado' });
+    }
+
+    tutor.pets.splice(petIndex, 1);
+    await tutor.save();
+
+    await Pet.findByIdAndDelete(petId);
+
+    res.status(204).json({ message: 'Pet removido com sucesso' });
   } catch (error) {
     res.status(500).json({ error: error });
   }
